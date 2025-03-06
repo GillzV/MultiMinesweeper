4513182de9d67eaf385964945710c190e68a67e4
GillzV <Gillv1027@gmail.com>
2025-03-06 15:56:32 -0800

    asdfasdf

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from app import app, socketio
 
 # Create WSGI app
-application = socketio.middleware(app)
+application = socketio.wrap_app(app)
 
 if __name__ == '__main__':
     socketio.run(app) 
