e0faa614acb2dbd66c061158d85f5778d8bd8c95
GillzV <Gillv1027@gmail.com>
2025-03-06 15:34:22 -0800

    New Upadte

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,6 @@
-from gevent import monkey
-monkey.patch_all()
-
 from app import app, socketio
 
-app = socketio.WSGIApp(app)
+application = socketio.WSGIApp(app)
 
 if __name__ == '__main__':
     socketio.run(app) 
