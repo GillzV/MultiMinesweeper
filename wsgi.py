1b944755dd2722d7b00e772ce465dcc73089de05
GillzV <GillV1027@gmail.com>
2025-04-10 23:45:02 -0700

    update for railway

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 from app import app, socketio
+import os
 
 # Create the WSGI application
 application = app
 
 if __name__ == '__main__':
-    socketio.run(app) 
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
